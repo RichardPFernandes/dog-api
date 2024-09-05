ef8a4f2c9598931e103d6951ac702926ede70c5d
Nicolas <nicolasbruski7@gmail.com>
2024-09-04 22:05:13 -0300

    feat(heart): adicionado corações ao card para favoritas

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,10 +1,15 @@
 import { BiSolidDownvote, BiSolidUpvote } from 'react-icons/bi'
+import { FaRegHeart } from "react-icons/fa";
 import './Card.css'
 
 export default function Card({ cachorro }) {
   return (
     <div className="cachorro">
       <div className="cachorro_info">
+        <div className="coracao">
+          <FaRegHeart color='red'/>
+        </div>
+        
         <img src={cachorro.image.url} alt={cachorro.name} />
         <p>Origem: {cachorro.origin}</p>
         <p>Raça: {cachorro.name}</p>
