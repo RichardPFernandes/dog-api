8d1ac0a2537d38bebd36c16a13770be7d25e274e
Nicolas <nicolasbruski7@gmail.com>
2024-09-09 19:53:53 -0300

    feat(card): adicionado botão de favoritos no card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,14 +1,29 @@
 import { BiSolidDownvote, BiSolidUpvote } from 'react-icons/bi'
-import { FaRegHeart } from "react-icons/fa";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 import './Card.css'
+import { useState } from 'react';
 
 export default function Card({ cachorro }) {
+  const [favorito, setFavorito] = useState(cachorro.favorito);
+
+  function adicionarFavorito() {
+    setFavorito(!favorito)
+  }
+
   return (
     <div className="cachorro">
       <div className="cachorro_info">
-        <div className="coracao">
-          <FaRegHeart color='red'/>
-        </div>
+        <button className="coracao" onClick={adicionarFavorito}>
+        {favorito ? (
+        <FaHeart
+          color="red"
+        />
+      ) : (
+        <FaRegHeart
+          color="red"
+        />
+      )}
+        </button>
         
         <img src={cachorro.image.url} alt={cachorro.name} />
         <p>Origem: {cachorro.origin}</p>
